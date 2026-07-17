29e490830c9f9d129af2557fcdc695e2040f9152
Anthony <tony920622@gmail.com>
2026-07-17 17:30:07 +0200

    Revert "Import bilingual Plasmic homepage"

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
 
 export default defineConfig({
   site: 'https://laraauditrisk.com',
-  build: { format: 'directory' },
-  integrations: [react()]
+  build: { format: 'directory' }
 });
